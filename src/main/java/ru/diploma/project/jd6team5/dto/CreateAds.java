eb34a587c3493e71d1f2ae294ee685661c2fb0cf
Maxim-Vagan <102387230+Maxim-Vagan@users.noreply.github.com>
2023-01-25 14:53:47 +0300

    Update CreateAds.java
    * Оптимизация аннотаций за счёт @Data

--- src/main/java/ru/diploma/project/jd6team5/dto/CreateAds.java
+++ src/main/java/ru/diploma/project/jd6team5/dto/CreateAds.java
@@ -4,10 +4,7 @@ import lombok.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
-@EqualsAndHashCode
-@ToString
+@Data
 public class CreateAds {
     private String description;
     private Float price;
